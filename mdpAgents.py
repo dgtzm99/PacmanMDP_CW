431ea45255c22123fb3de8303d6373e46594c533
dgtzm99 <52674748+dgtzm99@users.noreply.github.com>
2021-11-30 13:28:32 +0000

    Add files via upload

---
+++ mdpAgents.py
@@ -0,0 +1,316 @@
+# mdpAgents.py
+# parsons/20-nov-2017
+#
+# Version 1
+#
+# The starting point for CW2.
+#
+# Intended to work with the PacMan AI projects from:
+#
+# http://ai.berkeley.edu/
+#
+# These use a simple API that allow us to control Pacman's interaction with
+# the environment adding a layer on top of the AI Berkeley code.
+#
+# As required by the licensing agreement for the PacMan AI we have:
+#
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+# The agent here is was written by Simon Parsons, based on the code in
+# pacmanAgents.py
+
+from pacman import Directions
+from game import Agent
+import api
+import random
+import game
+import util
+
+
+class Grid:
+    #
+    # This class was taken from KEATS 6CCS3AIN Week 5
+    #  and slightly modified
+    #
+         
+    def __init__(self, width, height):
+        # Constructor
+        #
+        # Note that it creates variables:
+        #
+        # grid:   an array that has one position for each element in the grid.
+        # width:  the width of the grid
+        # height: the height of the grid
+        #
+        # Grid elements are not restricted, so you can place whatever you
+        # like at each location. You just have to be careful how you
+        # handle the elements when you use them.
+        
+        self.width = width
+        self.height = height
+        subgrid = []
+        for i in range(self.height):
+            row=[]
+            for j in range(self.width):
+                row.append(0)
+            subgrid.append(row)
+
+        self.grid = subgrid
+
+    def prettyDisplay(self): 
+        # The display function prints the grid out. 
+     
+        for i in range(self.height):
+            for j in range(self.width):
+                # print grid elements with no newline
+                print self.grid[self.height - (i + 1)][j],
+            # A new line after each line of the grid
+            print 
+        # A line after the grid
+        print
+        
+    def setValue(self, x, y, value):
+        # Set the values of specific elements in the grid.
+        # Here x and y are indices.
+
+        self.grid[y][x] = value
+
+    def getValue(self, x, y):
+        # Get the values of specific elements in the grid.
+        # Here x and y are indices.
+
+        return self.grid[y][x]
+
+    def getHeight(self):
+        # Return the height to support functions that manipulate the
+        # values stored in the grid.
+
+        return self.height
+
+    def getWidth(self):
+        # Return the width to support functions that manipulate the
+        # values stored in the grid.
+
+        return self.width
+
+# Value Iteration Values
+
+gamma = 0.6 # Discount factor
+reward_food = 2
+reward_ghost = -500
+reward_capsule = 4
+approximation_value = 0.00001
+iterations = 15
+
+class MDPAgent(Agent):
+    #
+    # MDP Agent 6CCS3AIN
+    # Made by David Gutierrez Moreno
+    # K-number = k19032572
+    #
+
+    def __init__(self):
+        # Constructor: this gets run when we first invoke pacman.py
+
+        print "Starting up MDPAgent!"
+        name = "Pacman"
+
+    def registerInitialState(self, state):
+        # Gets run after an MDPAgent object is created and once there is
+        # game state to access.
+
+        print "Running registerInitialState for MDPAgent!"
+        print "I'm at:"
+        print api.whereAmI(state)
+        self.makeMap(state)
+        self.addWallsToMap(state)
+        self.updateFoodInMap(state)
+        # self.updateCapsulesInMap(state)
+
+    def makeMap(self,state):
+        # Function taken from KEATS 6CCS3AIN Week 5
+        # Make a map by creating a grid of the right size
+
+        corners = api.corners(state)
+        height = self.getLayoutHeight(corners)
+        width  = self.getLayoutWidth(corners)
+        self.map = Grid(width, height)
+
+    def getLayoutHeight(self, corners):
+        # Function taken from KEATS 6CCS3AIN Week 5
+        # Returns height of the layout
+        
+        height = -1
+        for i in range(len(corners)):
+            if corners[i][1] > height:
+                height = corners[i][1]
+        return height + 1
+
+    def getLayoutWidth(self, corners):
+        # Function taken from KEATS 6CCS3AIN Week 5
+        # Returns width of the layout
+
+        width = -1
+        for i in range(len(corners)):
+            if corners[i][0] > width:
+                width = corners[i][0]
+        return width + 1
+
+    def addWallsToMap(self, state):
+        # Function taken from KEATS 6CCS3AIN Week 5
+        # Add walls '%' to the map.
+
+        walls = api.walls(state)
+        for i in range(len(walls)):
+            self.map.setValue(walls[i][0], walls[i][1], '%')
+
+    def updateFoodInMap(self, state):
+        # Function taken from KEATS 6CCS3AIN Week 5
+        # Update the food '*' in the map.
+        # 
+        # First, make all grid elements that aren't walls blank.
+        for i in range(self.map.getWidth()):
+            for j in range(self.map.getHeight()):
+                if self.map.getValue(i, j) != '%':
+                    self.map.setValue(i, j, 0)
+                    # self.map.setValue(i, j, ' ')
+        # food = api.food(state)
+        # for i in range(len(food)):
+        #     self.map.setValue(food[i][0], food[i][1], '*')
+
+    def updateCapsulesInMap(self, state):
+        # Updates the capsules 'o' in the map.
+
+        capsules = api.capsules(state)
+        for i in range(len(capsules)):
+            self.map.setValue(capsules[i][0], capsules[i][1], 'o')
+        
+    def final(self, state):
+        # This is what gets run in between multiple games
+
+        print "Looks like the game just ended!"
+
+    def getUtility(self, x, y, original_value):
+        # Returns Value of an Action, either the value of the coordinate
+        # or the value of the original coordinate in case of a wall
+        value = self.map.getValue(x, y)
+        if value == '%':
+            return original_value
+        else:
+            return value
+
+    def computeUtility(self, x, y, value, reward):
+        # Uses the Bellman update to compute the utility of
+        # the position (x,y) in the grid.
+        
+        # Actions to compute
+        up = self.getUtility(x, y+1, value)
+        down = self.getUtility(x, y-1, value)
+        left = self.getUtility(x-1, y, value)
+        right = self.getUtility(x+1, y, value)
+        
+        # Check if position contains a ghost,food,or pill
+
+        # As instructed in the document, probability of getting the 
+        #  action wanted is 0.8, going left is 0.1, and right is 0.1
+        up_util = 0.8*up + 0.1*left + 0.1*right
+        down_util = 0.8*down + 0.1*left + 0.1*right
+        left_util = 0.8*left + 0.1*up + 0.1*down
+        right_util = 0.8*right + 0.1*up + 0.1*down
+
+        # Reward + (discount factor * max(utility))
+        return reward + (gamma * max(up_util, down_util, left_util, right_util))
+
+
+    def computeValueIteration(self, state):
+        # Compute Value iteration with the current state
+
+        # Getting state info
+        ghost_pos = api.ghosts(state)
+        ghost_pos_time = api.ghostStatesWithTimes(state) # [((x,y),time),...])]
+        # pacman = api.whereAmI(state)
+        food = api.food(state)
+        capsules = api.capsules(state)
+        
+        for k in range(iterations):
+            newUtilityList = []
+            for i in range(self.map.getWidth()):
+                for j in range(self.map.getHeight()):
+                    value = self.map.getValue(i, j)
+                    if value != '%':
+                        if (i,j) in ghost_pos:
+                            reward = reward_ghost
+                        elif (i,j) in food:
+                            reward = reward_food
+                        elif (i,j) in capsules:
+                            reward = reward_capsule
+                        else:
+                            reward = -0.1
+                        newUtility = self.computeUtility(i, j, value, reward)
+                        newUtilityList.append(((i,j), newUtility))
+            # Update the map with the new utility values
+            for ((i,j), newUtility) in newUtilityList:
+                self.map.setValue(i, j, newUtility)
+
+    def getOptimalPolicy(self, state, legal):
+        # Returns optimal policy from the legal actions available
+        pacman = api.whereAmI(state)
+        pacX = pacman[0]
+        pacY = pacman[1]
+        origin_util = self.map.getValue(pacX, pacY)
+        up = self.getUtility(pacX, pacY+1, origin_util)
+        down = self.getUtility(pacX, pacY-1, origin_util)
+        left = self.getUtility(pacX-1, pacY, origin_util)
+        right = self.getUtility(pacX+1, pacY, origin_util)
+
+        action_utilities = []
+        for action in legal:
+            if action == Directions.NORTH:
+                up_util = 0.8*up + 0.1*left + 0.1*right
+                action_utilities.append((action, up_util))
+            if action == Directions.SOUTH:
+                down_util = 0.8*down + 0.1*left + 0.1*right
+                action_utilities.append((action, down_util))
+            if action == Directions.WEST:
+                left_util = 0.8*left + 0.1*up + 0.1*down
+                action_utilities.append((action, left_util))
+            if action == Directions.EAST:
+                right_util = 0.8*right + 0.1*up + 0.1*down
+                action_utilities.append((action, right_util))
+        return max(action_utilities, key = lambda i : i[1])[0]
+
+                    
+
+    def getAction(self, state):
+        # Iterates throughout the entire game, per action taken
+        #
+
+        # ghost_pos_time = api.ghostStatesWithTimes(state) # [((x,y),time),...])]
+        # pacman = api.whereAmI(state)
+        # food = api.food(state)
+        # capsules = api.capsules(state)
+
+        # No 'STOP' action allowed
+        legal = api.legalActions(state)
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+
+        # self.map.prettyDisplay()
+
+        # Start Value iteration process
+        self.computeValueIteration(state)
+
+        # Get the best action (optimal policy)
+        bestAction = self.getOptimalPolicy(state, legal)
+
+        # Random choice between the legal options.
+        return api.makeMove(bestAction, legal)
